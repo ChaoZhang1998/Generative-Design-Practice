0db430f1178c37e195e3cafb80375d6424f4bf9d
zhangchao <373930051@qq.com>
2020-12-09 14:23:56 +0800

    add P_1_0_01

--- Color/P_1_0_01/sketch.js
+++ Color/P_1_0_01/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
   // put setup code here
+  createCanvas(720, 720);
+
+  noCursor();
+
+  colorMode(HSB, 360, 100, 100);
+  rectMode(CENTER);
+
+  noStroke();
 }
 
 function draw() {
   // put drawing code here
+  background(mouseY / 2, 100, 100);
+
+  fill(360 - mouseY / 2, 100, 100); // color hue changes with the mouse moving vertically
+  rect(width / 2, height / 2, mouseX, mouseY); // rect size changes with the mouse moving
 }
